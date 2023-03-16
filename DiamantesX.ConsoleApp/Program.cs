1934f29613479dbaff95a489c9eedc945e314b29
bruno <bruno@DESKTOP-S7199VQ>
2023-03-16 16:50:49 -0300

    Add project files.

---
+++ DiamantesX.ConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Informe um número ímpar: ");
+        int num = int.Parse(Console.ReadLine());
+        int espacos = (num - 1) / 2;
+        int x = 1;
+
+        if(num % 2 == 0)
+        {
+            Console.WriteLine("[ERROR] Você informou um número par");
+        }
+        else { 
+
+        for (int l = 1; espacos > 0; l++)
+        {
+            for(int i = 1; i <= espacos; i++)
+            {
+                Console.Write(" ");
+            }
+            for(int j = 1; j <= x; j++)
+            {
+                Console.Write("x");
+            }
+            x += 2;
+            espacos--;
+            Console.WriteLine();
+        }
+        for (int l = 1; x > 0; l++)
+        {
+            for (int i = 1; i <= espacos; i++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 1; j <= x; j++)
+            {
+                Console.Write("x");
+            }
+            x -= 2;
+            espacos++;
+            Console.WriteLine();
+        }
+        }
+    }
+}
